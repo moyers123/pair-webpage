b8c4d3da65a9bec398c82ec4cdc7de1a9b518362
Eric Moy <eric.d.moy@gmail.com>
2022-02-16 11:08:34 -0800

    whack a mole

---
+++ index.js
@@ -0,0 +1,37 @@
+const holesArray = document.getElementsByClassName('hole')
+const board = document.getElementById('whack-a-mole')
+const score = document.getElementById('score')
+
+const delay = ms => new Promise(res => setTimeout(res, ms));
+let scoreCount = 0;
+
+const popUp = () => {
+    const indexNumber = Math.floor(Math.random() * 9);
+    const holePosition = holesArray[indexNumber];
+
+    if(holePosition.classList.contains('mole')){
+        return;
+    }
+    
+    holePosition.classList.toggle('mole');
+    setTimeout(() => {
+        //remove mole if not clicked on//
+        if(holePosition.classList.contains('mole'))
+        holePosition.classList.toggle('mole');
+    }, 2000)
+}
+
+const moleClick = (event) => {
+    if(event.target.classList.contains('mole')){
+        scoreCount++;
+        score.innerHTML = scoreCount;
+        event.target.classList.toggle('mole');
+    }
+    console.log(event.target);
+}
+
+board.addEventListener('click', moleClick)
+
+setInterval(popUp, 1000);
+
+
